2cf68fb739762f16bc22fd4ee526bb2b985b7f7b
Lesedi Maela <104493702+ImJustLes@users.noreply.github.com>
2025-03-19 18:13:45 +0200

    Update RecyclingTipService.java

--- src/main/java/com/enviro/assessment/grad001/lesedimaela/Waste/REST_API/api/service/RecyclingTipService.java
+++ src/main/java/com/enviro/assessment/grad001/lesedimaela/Waste/REST_API/api/service/RecyclingTipService.java
@@ -10,30 +10,38 @@ import java.util.Optional;
 @org.springframework.stereotype.Service
 public class RecyclingTipService extends Service<RecyclingTip>{
 
+    //Gets repo to make to perform CRUD operations
     @Autowired
     private RecyclingTipRepo recyclingTipRepo;
 
+    //Gets all Recyvling tips from the database.
     public List<RecyclingTip> getAll() {
         return recyclingTipRepo.findAll();
     }
 
+    //Gets the recycling tip, by id, from the database.
     public Optional<RecyclingTip> getByID(Long id) {
         return recyclingTipRepo.findById(id);
     }
 
+    //Deletes the recycling tip, by id, from the database.
     public void deleteByID(Long id) {
 
         recyclingTipRepo.deleteById(id);
     }
 
+    //Updates the tip, by id, with the new tip information into the databse.
     public RecyclingTip update(Long id, RecyclingTip newTip) {
 
         Optional<RecyclingTip> oldTip = getByID(id);
 
         if (oldTip.isPresent()) {
 
+            //Making the type of the tip lower case for input sanitisation
             newTip.setType(newTip.getType().toLowerCase());
             RecyclingTip updatedTip = oldTip.get();
+
+            //Replacing the old tip info with the new tip info.
             updatedTip.setTip(newTip.getTip());
             updatedTip.setType(newTip.getType());
 
@@ -44,8 +52,10 @@ public class RecyclingTipService extends Service<RecyclingTip>{
         return null;
     }
 
+    //Saves the recycling tip into the database.
     public RecyclingTip save(RecyclingTip newTip) {
 
+        //Making the type of the tip lower case for input sanitisation
         newTip.setType(newTip.getType().toLowerCase());
 
         RecyclingTip tipData = recyclingTipRepo.save(newTip);
