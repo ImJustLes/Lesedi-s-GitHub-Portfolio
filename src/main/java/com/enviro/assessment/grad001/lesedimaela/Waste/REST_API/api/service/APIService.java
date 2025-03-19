ed5dde8f7561c259d00abe3524d62cfd69f18c2f
Lesedi Maela <104493702+ImJustLes@users.noreply.github.com>
2025-03-19 18:26:13 +0200

    Update APIService.java

--- src/main/java/com/enviro/assessment/grad001/lesedimaela/Waste/REST_API/api/service/APIService.java
+++ src/main/java/com/enviro/assessment/grad001/lesedimaela/Waste/REST_API/api/service/APIService.java
@@ -2,9 +2,12 @@ package com.enviro.assessment.grad001.lesedimaela.Waste.REST_API.api.service;
 
 public interface APIService {
 
+    
     String apiVersion = "v2";
     String apiName = "environment";
-    String API_URL = "/" + apiVersion + "/" + apiName;
+
+    //URLs for requests.
+    String API_URL = "/" + apiVersion + "/" + apiName;    
     String SAVE_URL = "/save";
     String UPDATE_URL = "/update/{id}";
     String DELETE_URL = "/delete/{id}";
