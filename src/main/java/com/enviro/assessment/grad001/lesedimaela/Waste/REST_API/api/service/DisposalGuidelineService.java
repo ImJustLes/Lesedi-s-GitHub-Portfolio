8b8fcc3b829741ec0b6f335e7a9f504e690098c7
Lesedi Maela <104493702+ImJustLes@users.noreply.github.com>
2025-03-19 18:18:31 +0200

    Update DisposalGuidelineService.java

--- src/main/java/com/enviro/assessment/grad001/lesedimaela/Waste/REST_API/api/service/DisposalGuidelineService.java
+++ src/main/java/com/enviro/assessment/grad001/lesedimaela/Waste/REST_API/api/service/DisposalGuidelineService.java
@@ -11,29 +11,37 @@ import java.util.Optional;
 @Service
 public class DisposalGuidelineService extends com.enviro.assessment.grad001.lesedimaela.Waste.REST_API.api.service.Service<DisposalGuideline> {
 
+    //Gets the repo for the Guidelines
     @Autowired
     private DisposalGuidelineRepo guidelineRepo;
 
+    //Gets all the guidelines from the database.
     public List<DisposalGuideline> getAll() {
         return guidelineRepo.findAll();
     }
 
+    //Gets all the guidelines, by id, from the database.
     public Optional<DisposalGuideline> getByID(Long id) {
         return guidelineRepo.findById(id);
     }
 
+    //Deletes the guidelines, by id, from the database.
     public void deleteByID(Long id) {
         guidelineRepo.deleteById(id);
     }
 
+    //Updates the guideline, by idd, with the new guideline information.
     public DisposalGuideline update(Long id, DisposalGuideline newGuideline) {
 
         Optional<DisposalGuideline> oldGuideline = guidelineRepo.findById(id);
 
         if(oldGuideline.isPresent()){
 
-            newGuideline.setType(newGuideline.getType().toLowerCase());
+            //Making the type of guideline lower case for input sanitsation.
+            newGuideline.setType(newGuideline.getType().toLowerCase());            
             DisposalGuideline updatedGuideline = oldGuideline.get();
+
+            //Updating the old guideline with the new guideline information.
             updatedGuideline.setDescription(newGuideline.getDescription());
             updatedGuideline.setType(newGuideline.getType());
 
@@ -43,8 +51,12 @@ public class DisposalGuidelineService extends com.enviro.assessment.grad001.lese
         return null;
     }
 
+    //Saves the new guideline into the database.
     public DisposalGuideline save(DisposalGuideline guideline) {
 
+        //Making the type of guideline lower case for input sanitsation.
+        guideline.setType(guideline.getType().toLowerCase());  
+        
         DisposalGuideline newGuidelineData =  guidelineRepo.save(guideline);
         return newGuidelineData;
     }
